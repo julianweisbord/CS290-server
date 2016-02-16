017769d8d60dafaa95e4be8b661a2f228fc2b587
Bradley Imai <imaib@flip1.engr.oregonstate.edu>
2016-02-15 16:32:15 -0800

    added a few codes for database.js

--- server.js
+++ server.js
@@ -5,3 +5,28 @@ app.use(express.static('static'));
 app.listen(process.env.PORT || 8080);
 
 app.use(express.bodyParser());
+
+var path = require('path');
+var mongoose = require('mongoose');
+
+var app = express();
+
+//all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect('mongods://localhost/Company');
+
+var Schema = new mongoose.Schema({
+	_id : string,
+	name: string,
+	age : Number
+});
+
+	var user = mongoose.model('emp',Schema);
