1b8deec3e854bc9c4d28980d4a3ed7571610c624
blazerzero <habibelo@oregonstate.edu>
2016-02-15 21:16:21 -0800

    added a json file for now for where the books' info can go. we can decide whether or not to keep this file

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var app = express();
 app.use(express.static('static'));
 app.listen(process.env.PORT || 8080);
 
-app.use(express.bodyParser());
-
 var path = require('path');
 var mongoose = require('mongoose');
 
@@ -14,19 +12,24 @@ var app = express();
 //all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
 
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect('mongods://localhost/Company');
+mongoose.connect('mongods://localhost');
 
 var Schema = new mongoose.Schema({
-	_id : string,
-	name: string,
-	age : Number
+	subject: string,
+	course_number: Number,
+	book_title : string,
+  author: string,
+  price: Number,
+  isbn: string,
+  book_condition: string,
+  description: string
 });
 
 	var user = mongoose.model('emp',Schema);
