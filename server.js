a0f9871ebaccbca9a46dfe57bbf89cb884812658
blazerzero <habibelo@oregonstate.edu>
2016-02-18 15:27:40 -0800

    added the mongodb shell and stuff to the repo but if it's not necessary/good practice they can always be removed

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-var express = require('express');
+/*$.getScript("../CS290-client/booksell.html", function() {
+	console.log("booksell.html connected to table");
+});*/
+
 var app = express();
 
 app.use(express.static('static'));
@@ -19,17 +22,38 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect('mongods://localhost');
+mongoose.connect('mongodb://localhost/books');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
-var Schema = new mongoose.Schema({
-	subject: string,
-	course_number: Number,
-	book_title : string,
-  author: string,
-  price: Number,
-  isbn: string,
-  book_condition: string,
-  description: string
-});
+db.once('open', function() {
+	var bookSchema = new mongoose.Schema({
+		subject: string,
+		course_number: Number,
+		book_title : string,
+  	author: string,
+  	price: Number,
+  	isbn: string,
+  	book_condition: string,
+  	description: string
+	});
 
-	var user = mongoose.model('emp',Schema);
+	var book = mongoose.model('book',bookSchema);
+
+	var listing = new book({
+		subject: document.getElementById("subject").value;
+		course_number: document.getElementById("coursenum").value;
+		book_title: document.getElementById("booktitle").value;
+		author: document.getElementById("author").value;
+		price: document.getElementById("price").value;
+		isbn: document.getElementById("isbn").value;
+		if (document.getElementById("condition").value == 4) book_condition: "Excellent";
+		if (document.getElementById("condition").value == 3) book_condition: "Good";
+		if (document.getElementById("condition").value == 2) book_condition: "Fair";
+		if (document.getElementById("condition").value == 1) book_condition: "Poor";
+		description: document.querySelector(".ui.radio.checkbox:checked").value;
+	})
+
+	listing.save(function (err) {
+		if (err) return console.error(err);
+	})
